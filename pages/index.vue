24bfe0bd1323339d4aad3e5baa234afd49fdec29
Abdallah Bratos <bratosab@gmail.com>
2019-11-13 16:56:38 +0100

    Added vuex store and moved posts to module

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,33 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-fundamentals
-      </h1>
-      <h2 class="subtitle">
-        My smashing Nuxt.js project
-      </h2>
+      <h1 class="title">nuxt-fundamentals</h1>
+      <h2 class="subtitle">My smashing Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
+        <nuxt-link
+          :key="post.id"
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
           class="button--grey"
-        >
-          GitHub
-        </a>
+        >{{post.title}}</nuxt-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -46,8 +39,8 @@ export default {
   text-align: center;
 }
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
